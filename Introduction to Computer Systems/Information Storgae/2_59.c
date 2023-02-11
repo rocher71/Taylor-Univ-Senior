6a948df6828327511086e5490a025856629731f2
Yejin Kim <qja086351@naver.com>
2023-02-11 17:40:07 -0500

    assignment2-1st

---
+++ Introduction to Computer Systems/Information Storgae/2_59.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+typedef unsigned char *byte_poninter;
+
+void show_bytes(byte_poninter start, size_t len){
+    int i;
+    for(i =0; i<len; i++)
+        printf(" %.2x", start[i]);
+    printf("\n");
+}
+
+void show_short(short x){
+    show_bytes((byte_poninter) &x, sizeof(short ));
+}
+
+void show_long(long x){
+    show_bytes((byte_poninter) &x, sizeof(long ) );
+}
+
+void show_doulbe(double x){
+    show_bytes((byte_poninter) &x, sizeof(double ) );
+}
+
+int main(){
+    short s;
+    long l;
+    double d;
+    printf("short : ");
+    scanf("%hd", &s);
+
+    printf("long : ");
+    scanf("%ld", &l);
+
+    printf("double : ");
+    scanf("%lf", &d);
+
+    show_short(s);
+    show_long(l);
+    show_doulbe(d);
+}
